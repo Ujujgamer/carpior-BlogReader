67286fc72947421e483d447c4da74f88cdc6d269
Rolando Carpio <jujucarpio@sbcglobal.net>
2014-10-02 14:27:34 -0700

    Created a blog post class.

---
+++ BlogReader/app/src/main/java/android/carpior/blogreader/BlogPost.java
@@ -0,0 +1,14 @@
+package android.carpior.blogreader;
+
+/**
+ * Created by Student on 10/2/2014.
+ */
+public class BlogPost {
+    public final String title;
+    public final String url;
+
+    public BlogPost(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+}
