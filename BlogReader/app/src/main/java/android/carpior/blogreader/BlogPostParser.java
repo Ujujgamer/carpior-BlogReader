9b7f1f1705a9b3449204ea9b1d91db1f2ff6ab58
Rolando Carpio <jujucarpio@sbcglobal.net>
2014-10-13 14:45:34 -0700

    Created variables for parsing

--- BlogReader/app/src/main/java/android/carpior/blogreader/BlogPostParser.java
+++ BlogReader/app/src/main/java/android/carpior/blogreader/BlogPostParser.java
@@ -3,7 +3,9 @@ package android.carpior.blogreader;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class BlogPostParser {
     private static BlogPostParser parser;
@@ -20,6 +22,10 @@ public class BlogPostParser {
     }
 
     public JSONObject parse(InputStream inputStream) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder builder = new StringBuilder();
+        JSONObject jsonObject = null;
+        
         return null;
     }
 }
