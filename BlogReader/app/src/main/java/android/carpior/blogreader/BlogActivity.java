be77b5204fd0956a3ddb650a5ff73e4773e1313e
Rolando Carpio <jujucarpio@sbcglobal.net>
2014-10-15 21:10:29 -0700

    Set the adapter within BlogPostTask

--- BlogReader/app/src/main/java/android/carpior/blogreader/BlogActivity.java
+++ BlogReader/app/src/main/java/android/carpior/blogreader/BlogActivity.java
@@ -17,20 +17,10 @@ public class BlogActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
 
-        String[] arrayStrings = new String[] {
-                "Blog post 1",
-                "Blog post 2",
-                "Blog post 3",
-                "Blog post 4"
-        };
-
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
         listView = (ListView)findViewById(R.id.ListView);
 
         listView.setEmptyView(progressBar);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayStrings);
-        listView.setAdapter(adapter);
 
         new BlogPostTask().execute();
     }
